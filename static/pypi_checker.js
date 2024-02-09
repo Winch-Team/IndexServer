4e78d208535b372cdb9848bf450faf60248fd4b1
Brody Critchlow <brody@tntsplash.net>
2024-02-08 22:11:22 -0700

    Pause version checking

--- static/pypi_checker.js
+++ static/pypi_checker.js
@@ -5,25 +5,22 @@ function semverCompare(a, b) {
 }
 
 function check_supply_chain_attack(package_name, package_version, callback_if_unsafe) {
-    $.ajax({
-        type: 'GET',
-        crossDomain: true,
-        dataType: 'json',
-        url: `https://pypi.org/pypi/${package_name}/json`,
-        error: function(response) {
-            console.log(`Couldn't find ${package_name} on PyPi : you are safe from supply chain attacks.`);
-        },
-        success: function(jsondata){
-            // If the package exists, ensure our package has a higher version
-            // And if it doesn't have a higher version, call the callback so that
-            // we can take action
-            var pypi_vers = jsondata['info']['version'];
+    // $.ajax({
+    //     type: 'GET',
+    //     crossDomain: true,
+    //     dataType: 'json',
+    //     url: `https://winchteam.dev/wpi/${package_name}/json`,
+    //     error: function(response) {
+    //         console.log(`Couldn't find ${package_name} on WPI : you are safe from supply chain attacks.`);
+    //     },
+    //     success: function(jsondata){
+    //         var pypi_vers = jsondata['info']['version'];
 
-            if (semverCompare(package_version, pypi_vers) <= 0) {
-                callback_if_unsafe();
-            } else {
-                console.log(`${package_name} exists on PyPi, but the version is lower (${package_version} > ${pypi_vers}), so you are safe from supply chain attacks.`)
-            }
-        }
-    })
+    //         if (semverCompare(package_version, pypi_vers) <= 0) {
+    //             callback_if_unsafe();
+    //         } else {
+    //             console.log(`${package_name} exists on PyPi, but the version is lower (${package_version} > ${pypi_vers}), so you are safe from supply chain attacks.`)
+    //         }
+    //     }
+    // })
 }
